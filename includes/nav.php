c85a71a22e2cd12de8fdf2e4c5c3e868d72b2568
CasMeijer2005 <90309791+CasMeijer2005@users.noreply.github.com>
2022-02-11 20:24:06 +0100

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,3 +1,5 @@
+<?php require_once "includes/bd.php" ?>
+
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -13,15 +15,18 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
+
+                <?php
+                $query = "SELECT * from categories";
+                $result = mysqli_query($con, $query);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $cat_title = $row['cat_title'];
+                    echo " <li>
+                            <a href='#'>{$cat_title}</a>
+                            </li>";
+                }
+
+                ?>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
