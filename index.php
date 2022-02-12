9ff2d0d3aaba682d4f1962182a82c0738cf6ca52
CasMeijer2005 <90309791+CasMeijer2005@users.noreply.github.com>
2022-02-12 16:33:07 +0100

    photos dinamisch in beeld

--- index.php
+++ index.php
@@ -9,22 +9,22 @@
 
     <div class="row">
 
-        <?php
-        $query = "SELECT * from posts";
-        $data = mysqli_query($con, $query);
 
-        while ($row = mysqli_fetch_assoc($data)) {
-            $Post_title = $row['post_title'];
-            $Post_author = $row['post_author'];
-            $Post_date = $row['post_date'];
-            // $Post_img = $row['post_img'];
-            $Post_content = $row['post_content'];
-            $Post_tags = $row['post_tags'];
-        ?>
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+            <?php
+            $query = "SELECT * from posts";
+            $data = mysqli_query($con, $query);
 
+            while ($row = mysqli_fetch_assoc($data)) {
+                $Post_title = $row['post_title'];
+                $Post_author = $row['post_author'];
+                $Post_date = $row['post_date'];
+                $Post_img = $row['post_img'];
+                $Post_content = $row['post_content'];
+                $Post_tags = $row['post_tags'];
+            ?>
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -38,18 +38,19 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $Post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="imgs/<?php echo $Post_img ?>" alt="">
                 <hr>
                 <p><?php echo $Post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-            </div>
-        <?php
+            <?php
 
-        }
+            }
 
-        ?>
+            ?>
+
+        </div>
 
         <!-- side bar -->
         <?php require_once "includes/sidebar.php" ?>;
